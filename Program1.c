b6c67ea3a0268295df9571fdadbece246647bb5e
SanketNBhosale <86365665+SanketNBhosale@users.noreply.github.com>
2021-06-23 20:12:43 +0530

    Add files via upload

---
+++ Program1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Jay Ganesh\n");
+    
+    return 0;
+}
+
+// ./Myexe -> OS -> main()  -> end of main -> return 0 to OS (it indicates success)
+
+// return from main to opertaing system
+// return 0     successful termitnation
+// return 1     abortive termination
+// return -1    erronious termination
